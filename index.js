c33b4d9d8bcba9872859c86db8c7fd4ff16fad2e
raeRaweq <66205353+raeRaweq@users.noreply.github.com>
2020-05-31 14:24:22 +0200

    bot1

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+const PREFIX  = '#';
+
+var version = '1.0'
+
+bot.on('ready', () =>{
+    console.log('FlugBot hebt ab!')
+})
+
+bot.on('message', message=>{
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'youtube':
+            message.reply('Flugarmy auf YouTube : https://www.youtube.com/channel/UCZi1ytnX4O-BIribX5I8O9w');
+            break;
+        case 'uploadplan':
+            message.reply('Jeden 2. Tag gibt es Highquality Content von Flugarmy!');
+            break;
+        case 'info':
+            if(args[1] === 'version'){
+                message.channel.send('Version ' + version + ' by Raweq');
+            }else{
+                message.reply('ungültiger Befehl')
+            }
+            break;
+        case 'clear':
+            if(!args[1]) return message.reply ('Ups! Es ist ein Fehler unterlaufen!(#clear [nummer]')
+            message.channel.bulkDelete(args[1]);
+            break;
+        
+
+    }
+})
+
+bot.login(process.env.token);
