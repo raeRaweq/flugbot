f3890238cf105b3c136265e52f6549ba74742607
raeRaweq <66205353+raeRaweq@users.noreply.github.com>
2020-06-02 01:10:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 
-const PREFIX  = '#';
+const PREFIX = '#';
 
 var version = '1.1'
 
-bot.on('ready', () =>{
+bot.on('ready', () => {
     console.log('FlugBot hebt ab!')
 })
 
 bot.on('message', message => {
     if (!message.guild) return;
+    if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("Dafür hast du keine Rchte!")
 
     if (message.content.startsWith('#kick')) {
         const user = message.mentions.users.first();
@@ -19,14 +20,14 @@ bot.on('message', message => {
             const member = message.guild.member(user);
             if (member) {
                 member
-                .kick('Grund')
-                .then(() => {
-                    message.reply(`Erfolgreich ${user.tag} gekickt.`);
-                })
-                .catch(err => {
-                    message.reply('ich konnte dieses Mitglied nicht kicken');
-                    console.error(err);
-                });
+                    .kick('Grund')
+                    .then(() => {
+                        message.reply(`Erfolgreich ${user.tag} gekickt.`);
+                    })
+                    .catch(err => {
+                        message.reply('ich konnte dieses Mitglied nicht kicken');
+                        console.error(err);
+                    });
             } else {
                 message.reply("Dieser User ist nicht im!");
             }
@@ -37,11 +38,11 @@ bot.on('message', message => {
 });
 
 
-bot.on('message', message=>{
+bot.on('message', message => {
 
     let args = message.content.substring(PREFIX.length).split(" ");
 
-    switch(args[0]){
+    switch (args[0]) {
         case 'youtube':
             message.reply('Flugarmy auf YouTube : https://www.youtube.com/channel/UCZi1ytnX4O-BIribX5I8O9w');
             break;
@@ -49,17 +50,17 @@ bot.on('message', message=>{
             message.reply('Jeden 2. Tag gibt es Highquality Content von Flugarmy!');
             break;
         case 'info':
-            if(args[1] === 'version'){
+            if (args[1] === 'version') {
                 message.channel.send('Version ' + version + ' by Raweq');
-            }else{
+            } else {
                 message.reply('ungültiger Befehl')
             }
             break;
         case 'clear':
-            if(!args[1]) return message.reply ('Ups! Es ist ein Fehler unterlaufen!(#clear [nummer]')
+            if (!args[1]) return message.reply('Ups! Es ist ein Fehler unterlaufen!(#clear [nummer]')
             message.channel.bulkDelete(args[1]);
             break;
-        
+
 
     }
 })
