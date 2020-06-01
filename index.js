5ab948d87f7238bd19a49824d2e500d4bca89b37
raeRaweq <66205353+raeRaweq@users.noreply.github.com>
2020-06-02 00:46:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ bot.on('ready', () =>{
     console.log('FlugBot hebt ab!')
 })
 
-bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'eingangshalle');
-    if (!channel) return;
+bot.on('message', message => {
+    if (!message.guild) return;
 
-    channel.send(`Willkommen auf dem Server, ${member},bitte lies dir die #regeln druch!`)
+    if (message.content.startsWith('!kick')) {
+        const user = message.mentions.users.first();
+        if (user) {
+            const member = message.guild.member(user);
+            if (member) {
+                member
+                .kick('Optional reason that will display in the audit logs')
+                .then(() => {
+                    message.reply(`Successfully kicked ${user.tag}`);
+                })
+                .catch(err => {
+                    message.reply('I was unable to kick the member');
+                    console.error(err);
+                });
+            } else {
+                message.reply("That user isn't in this guild!");
+            }
+        } else {
+            message.reply("You didn't mention the user to kick!");
+        }
+    }
 });
 
 
